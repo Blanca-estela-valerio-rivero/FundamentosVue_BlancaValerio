bc833009f9bff6f42fcaa3bd124b360014b59597
Blanca-estela-valerio-rivero <valerioblancaestela@gmail.com>
2025-03-20 17:34:38 -0600

    Subiendo Carpeta inicial

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,58 @@
+<script setup>
+//metodos
+</script>
+
+<template>
+  <!-- estructura html -->
+
+   
+   <h2>  IA</h2>
+   <img src="https://neuroflash.com/wp-content/uploads/2023/05/IA-que-reconoce-imagenes-imageflash.png" alt="Descripción" width="300">
+    <p> La IA permite a las máquinas aprender y resolver problemas de forma inteligente.</p>
+    <button class="btn">IA </button>
+    <button class="boton2">INICIAR </button>
+   
+
+</template>
+/* estilos csss */
+<style scoped> 
+
+h1{
+  font-family: 'Courier New', Courier, monospace;
+  font-size: 45px;
+  color: blue;
+}
+.btn{
+
+  border-radius: 15px;
+  background-color: aquamarine;
+  color: azure;
+  font-family: 'Gill Sans', 'Gill Sans MT', Calibri, 'Trebuchet MS', sans-serif;
+
+}
+
+
+
+h2{
+font-family: 'Times New Roman', Times, serif;
+font-size: 45px;
+color: rgb(83, 170, 32);
+}
+.boton{
+
+border-radius: 15px;
+background-color:rgb(125, 19, 146);
+color: blueviolet;
+font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
+
+}
+
+.boton2{
+
+border-radius: 15px;
+background-color: aquamarine;
+color: rgb(160, 22, 68);
+font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
+
+}
+</style>
