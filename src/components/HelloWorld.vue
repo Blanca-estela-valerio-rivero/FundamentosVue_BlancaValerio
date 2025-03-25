d857e1577a882e054b761b227fdcfd0b3a6f8125
Blanca-estela-valerio-rivero <valerioblancaestela@gmail.com>
2025-03-24 18:45:05 -0600

    Importe el card en el script y dentro   del template

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,5 +1,5 @@
 <script setup>
-
+import Card from './Card.vue';
 </script>
 
 <template>
@@ -44,51 +44,9 @@
       </div>
     </div>
 
-    <div class="row mt-3">
-      <div class="col-md-3">
-        <div class="card" style="width: 18rem;">
-          <img src="https://www.ejemplos.co/wp-content/uploads/2015/05/elefante-herbivoro-e1475100795272.jpg" class="card-img-top" alt="...">
-          <div class="card-body text-center">
-            <p class="card-text"> Es el mamífero terrestre más grande.
-               Pueden llegar a pesar más de 7 mil kilos y aunque en promedio suelen medir tres metros de altura. Algunos elefantes llegan a vivir 90 años.
-                Son capaces de comunicarse a través de vibraciones en el suelo.
-            </p>
-              <button class="btn btn-warning">Más información</button>
-          </div>
-        </div>
-      </div>
-      <div class="col-md-3">
-        <div class="card" style="width: 18rem;">
-          <img src="https://conexion.puce.edu.ec/wp-content/uploads/2022/10/Ballena-saltando-1024x675.jpg" class="card-img-top" alt="...">
-          <div class="card-body text-center">
-            <p class="card-text"> Es un cetáceo, es decir un mamífero adaptado a la vida acuática. 
-              A diferencia de los peces, los cetáceos tienen respiración pulmonar. 
-              Tienen un cuerpo similar al de los peces, porque ambos tienen formas hidrodinámicas.</p>
-              <button class="btn btn-warning">Más información</button>
-          </div>
-        </div>
-      </div>
-      <div class="col-md-3">
-        <div class="card" style="width: 18rem;">
-          <img src="https://files.worldwildlife.org/wwfcmsprod/images/Polar_Bear_and_Baby_OGC_last_chance_2016/story_full_width/5ycavivm2p_2016_last_chance_hero_v4.jpeg" class="card-img-top" alt="...">
-          <div class="card-body text-center">
-            <p class="card-text">Uno de los mamíferos terrestres más grandes que existen. 
-              Vive en las zonas heladas del hemisferio norte. 
-              Su cuerpo está adaptado a las bajas temperaturas gracias a diversas capas de pelo y grasa.</p>
-              <button class="btn btn-warning">Más información</button>
-          </div>
-        </div>
-      </div>
-      <div class="col-md-3">
-        <div class="card" style="width: 18rem;">
-          <img src="https://www.nationalgeographic.com.es/medio/2021/03/16/mm7685-080119-08101_b6032858_1500x1481.jpg" class="card-img-top" alt="...">
-          <div class="card-body text-center">
-            <p class="card-text">El murciélago es uno de esos animales fascinantes que nos regala la naturaleza.
-               Es el único mamífero con la posibilidad de volar por sí solo, expandiendo sus alas y consiguiendo alzar el vuelo en un momento. .</p>
-              <button class="btn btn-warning">Más información</button>
-          </div>
-        </div>
-      </div>
+    
+    <div>
+    <Card/>
     </div>
 
 
