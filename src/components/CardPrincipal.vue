c28a19d81a9e7e1f6299cadc2bf4c3f157f7ed37
Blanca-estela-valerio-rivero <valerioblancaestela@gmail.com>
2025-03-24 19:05:28 -0600

    Agregue estructura al card principal e importe el componente a helloworld

---
+++ src/components/CardPrincipal.vue
@@ -0,0 +1,41 @@
+<template>
+   
+    <h1 class="text-center">ANIMALES MAMIFEROS</h1>
+    <div class="row">
+      <div class="col-md-6">
+        <div class="card">
+          <div class="card-header">
+            <h2>DELFIN</h2>
+            <img width="250px" height="200px" style="display: block; margin: auto; border-radius: 15%;"
+              src="http://www.fotosanimales.es/wp-content/uploads/2015/12/delfin-saltando.jpg" alt="">
+          </div>
+          <div class="card-body">
+            <p class="card-text">Los delfines son mamíferos marinos pertenecientes a la familia Delphinidae dentro del
+              orden Cetacea.
+              Son conocidos por su inteligencia, comportamiento social y habilidades de comunicación.
+              se alimentan principalmente de peces, calamares y crustáceos.
+              Cazan en grupo y utilizan estrategias inteligentes, como el uso de burbujas para atrapar peces.
+            </p>
+          </div>
+        </div>
+      </div>
+      <div class="col-md-6">
+        <div class="card">
+          <div class="card-header">
+            <h2>LEON</h2>
+            <img width="250px" height="200px" style="display: block; margin: auto; border-radius: 15%;"
+              src="https://www.dondevive.org/wp-content/uploads/2015/07/leon.jpg" alt="">
+          </div>
+          <div class="card-body">
+            <p class="card-text">Mamífero felino que vive en África subsahariana y en el noroeste de la India.
+              Es una especie en peligro, por eso muchos ejemplares viven en reservas.
+              Es un animal carnívoro, depredador principalmente de otros mamíferos grandes como ñus,
+              impalas, cebras, búfalos, nilgós, jabalíes y ciervos. Para alimentarse de estos animales, suelen cazar en
+              grupos.
+
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+</template>
